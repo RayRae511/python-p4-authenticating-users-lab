de1218c950a5c3045fe4aff5cddd6c84df503694
Raymond Oeta <oetaray@gmail.com>
2023-09-30 18:14:31 +0300

    completed wrk

--- server/app.py
+++ server/app.py
@@ -18,40 +18,27 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return make_response(jsonify(user.username), 200)
+        return make_response(jsonify('Invalid credentials')), 401
 
+class Logout(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        session.pop('user_id', None)
+        return make_response(jsonify(''), 204)
 
-        return {}, 204
-
-class IndexArticle(Resource):
-    
+class CheckSession(Resource):
     def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
+        return make_response(jsonify(session.get('user_id')), 200)
 
-class ShowArticle(Resource):
-
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
-
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')                      
+        
